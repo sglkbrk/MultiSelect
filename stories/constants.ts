889c2e290c42a0209ae204e2157e518cdc62baee
burak <sglk.brk@gmail.com>
2024-03-14 22:12:17 +0300

    episode düzenleme

--- stories/constants.ts
+++ stories/constants.ts
@@ -1,9 +1,9 @@
 export const items = [
-  { key: "Option 1", cat: "Group 1" },
-  { key: "Option 2", cat: "Group 1" },
-  { key: "Option 3", cat: "Group 1" },
-  { key: "Option 4", cat: "Group 2" },
-  { key: "Option 5", cat: "Group 2" },
-  { key: "Option 6", cat: "Group 2" },
-  { key: "Option 7", cat: "Group 2" },
+  { key: "Option 1", cat: "Group 1", episode: [] },
+  { key: "Option 2", cat: "Group 1", episode: [] },
+  { key: "Option 3", cat: "Group 1", episode: [] },
+  { key: "Option 4", cat: "Group 2", episode: [] },
+  { key: "Option 5", cat: "Group 2", episode: [] },
+  { key: "Option 6", cat: "Group 2", episode: [] },
+  { key: "Option 7", cat: "Group 2", episode: [] },
 ];
